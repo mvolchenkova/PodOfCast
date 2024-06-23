e55dbb4375de2b609a0c04c5cf3eba5dcc2d60dd
mvolchenkova <121573011+mvolchenkova@users.noreply.github.com>
2024-06-24 00:35:37 +0300

    feat: add realizatuon fot the first slider

--- index/index.js
+++ index/index.js
@@ -45,3 +45,40 @@ const recent = document.querySelector('.recentButton');
 recent.addEventListener('click', function(){
     window.location = '../index/index.html#sixthContainer'
 })
+
+let currentSlide = 0;
+const slider = document.querySelector('#slider');
+const slides = slider.children;
+let slideWidth;
+
+// Определение ширины слайда в зависимости от ширины экрана
+if (window.innerWidth > 768) {
+    slideWidth = 400; // ширина слайда для экранов больше 768px
+} else {
+    slideWidth = 220; // ширина слайда для экранов меньше 768px
+}
+
+const prevButton = document.querySelector('.leftButton');
+const nextButton = document.querySelector('.rightButton');
+
+nextButton.addEventListener('click', function(){
+    // Переходим к следующему слайду
+    currentSlide = currentSlide + 1;
+    // Ограничиваем переходы слайдов до последнего слайда
+    if(currentSlide >= slides.length) {
+        currentSlide = slides.length - 1;
+    }
+    // Смещаем слайдер
+    slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+});
+
+prevButton.addEventListener('click', function(){
+    // Переходим к предыдущему слайду
+    currentSlide = currentSlide - 1;
+    // Ограничиваем переходы слайдов до первого слайда
+    if(currentSlide < 0) {
+        currentSlide = 0;
+    }
+    // Смещаем слайдер
+    slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+});
